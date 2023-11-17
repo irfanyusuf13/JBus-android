662d5cd535593632e9dfe58a2c4b36a50dd82d5a
Irfan <irfanyusufk13@gmail.com>
2023-11-17 13:48:24 +0700

    CS08 Menyelesaikan CS modul 8

--- app/src/main/java/com/irfanYusufJBusRA/jbus_android/LoginActivity.java
+++ app/src/main/java/com/irfanYusufJBusRA/jbus_android/LoginActivity.java
@@ -2,13 +2,43 @@ package com.irfanYusufJBusRA.jbus_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView signUp = null;
+    private Button loginButton = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signUp = findViewById(R.id.signup);
+        loginButton = findViewById(R.id.Register);
+
+        signUp.setOnClickListener(v -> {
+            moveActivity(this, RegisterActivity.class);
+        });
+        loginButton.setOnClickListener(v ->{
+            moveActivity(this, MainActivity.class);
+        });
+
     }
+    private void moveActivity(Context ctx, Class<?> cls){
+        Intent intent = new Intent(ctx , cls);
+        startActivity(intent);
+    }
+    private void viewToast(Context ctx, String message){
+        Toast.makeText(ctx , message , Toast.LENGTH_SHORT).show();
+    }
+
+
 }
---
+++ app/src/main/java/com/irfanYusufJBusRA/jbus_android/MainActivity.java
@@ -0,0 +1,21 @@
+package com.irfanYusufJBusRA.jbus_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
+}
